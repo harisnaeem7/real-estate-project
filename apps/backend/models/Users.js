0ff4a5c975c15e10fe105b71bccab9e162bce2f6
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-11-06 18:23:07 -0700

    created user model

---
+++ apps/backend/models/Users.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, description: "User name" },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["admin", "seller", "buyer"], default: "buyer" },
+  phone: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("User", userSchema);
